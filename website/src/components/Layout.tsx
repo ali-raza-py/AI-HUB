bbee6f7d056573ffe3c0e57b2050b05cac7155f2
Ali Raza <btwaliraza110@gmail.com>
2026-09-14 01:03:48 +0500

    Fix light mode navigation contrast

--- website/src/components/Layout.tsx
+++ website/src/components/Layout.tsx
@@ -6,7 +6,7 @@ export default function Layout() {
   return (
     <div className="app-shell min-h-screen flex flex-col">
       <Navbar />
-      <main className="flex-1 relative">
+      <main className="app-main flex-1 relative">
         <Outlet />
       </main>
       <Footer />
